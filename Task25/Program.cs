854c357ff69b77d6d106c37c2b7254804ae94a6c
zhyk <zhyk@list.ru>
2023-10-24 10:36:36 +0300

    Задача № 1

---
+++ Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// ПОКА НАПИСАНА ФУНКЦИЯ ВВОДА ВЕЩЕСТВЕННОГО ЧИСЛА С ПРОВЕРКОЙ НА
+// НА КОРРЕКТНОСТЬ ВВОДА
+
+
+double InputDouble(string name)
+{
+    Console.Write($"Введите число {name}: ");
+    if (double.TryParse(Console.ReadLine(), out double number))
+    {
+        Console.WriteLine($"А = {number}");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введите число");
+    }
+    return number;
+}
+
+
+
+double A = InputDouble("A");
+double B = InputDouble("B");
+
+
+
+//double A = Convert.ToDouble(Console.Read());
+
+
+
+
---
+++ Task25_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+
+
+int InputInt(string name)
+{
+    Console.Write($"Введите число {name}: ");
+    if (int.TryParse(Console.ReadLine(), out int number))
+    {
+        Console.WriteLine($"А = {number}");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введите число");
+    }
+    return number;
+}
+
+int Exponent(int num, int exp)
+{
+    int res = 1;
+   for (int i = 1; i <= exp; i++)
+   {
+    res *= num;
+   } 
+   return res;
+}
+
+int A = InputInt("A");
+int B = InputInt("B");
+int result = Exponent(A, B);
+Console.WriteLine($"А в степени В = {result}");
+
+
+
+
+
+
+
+
